7114b07fbbf9ed8d9671d912b35719504227d73c
rahul19106070 <100035393+rahul19106070@users.noreply.github.com>
2022-08-03 21:33:17 +0530

    Time: 129 ms (19.08%), Space: 54.4 MB (58.86%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -1,5 +1,5 @@
 class MyCalendar {
-    private List<int[]> books = new ArrayList<>();
+    public List<int[]> books = new ArrayList<>();
     public boolean book(int start, int end) {
         for (int[] b : books)
             if (Math.max(b[0], start) < Math.min(b[1], end)) return false;
